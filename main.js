86fe368c4d0ce590546d60688f1eeba64d1467cd
Sadhasivam <iamstarksiv@gmail.com>
2022-11-18 12:14:49 +0530

    updated new web design

---
+++ main.js
@@ -0,0 +1,11 @@
+let slideup = document.getElementById("slide-up-id");
+
+function slideUpWork()
+{
+    slideup.classList.add("slide-up");
+}
+
+function slideDown()
+{
+    slideup.classList.remove("slide-up");
+}
