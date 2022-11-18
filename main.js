f968a35374d9625a714d160b7bc08eb975c56f1a
Sadhasivam <iamstarksiv@gmail.com>
2022-11-18 16:26:51 +0530

    add pointer cursor to work close fixed screen scroll for body when work window is active

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 let slideup = document.getElementById("slide-up-id");
-
+let nonScroll = document.getElementById("home");
 function slideUpWork()
 {
     slideup.classList.add("slide-up");
+    nonScroll.classList.add("non-scroll");
 }
 
 function slideDown()
 {
     slideup.classList.remove("slide-up");
+    nonScroll.classList.remove("non-scroll");
 }
+
